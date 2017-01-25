d18ee7a067e6665eae83dabdf335f686808dc33c
David <dvd.brtrnd@gmail.com>
2017-01-25 16:47:51 +0100

    tests sans Objets cote serveur (suite)  - et avec mapper mutualisé l- uniformisation Find et FindOne

--- app/controllers/GetElements.java
+++ app/controllers/GetElements.java
@@ -3,6 +3,7 @@ package controllers;
 import models.*;
 import org.bson.types.ObjectId;
 import org.jongo.Find;
+import org.jongo.FindOne;
 import org.jongo.MongoCursor;
 import play.mvc.*;
 
@@ -84,7 +85,7 @@ public class GetElements extends Controller {
 
         MongoAccess.connect();
 
-        return ok(new ObjectMapper().readTree(MongoAccess.request(collection, new ObjectId(id)).toString()));
+        return ok(new ObjectMapper().readTree(mapping(MongoAccess.request(collection, new ObjectId(id)), collection).toString()));
 
 
     }
@@ -207,6 +208,69 @@ public class GetElements extends Controller {
         return ok(new ObjectMapper().readTree(MongoAccess.request(collection, champ1, valeur1, champ2, valeur2).toString()));
 
     }
+
+    public String mapping(FindOne find, String collection) {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        String stringResult = "";
+
+        try {
+
+            switch (collection){
+
+                case "auteur" : Auteur m_auteur = find.as(Auteur.class);
+                    stringResult = mapper.writeValueAsString(m_auteur);
+                    break;
+                case "client" : Client m_client = find.as(Client.class);
+                    stringResult = mapper.writeValueAsString(m_client);
+                    break;
+                case "commande" : Commande m_commande = find.as(Commande.class);
+                    stringResult = mapper.writeValueAsString(m_commande);
+                    break;
+                case "complement" : Complement m_complement = find.as(Complement.class);
+                    stringResult = mapper.writeValueAsString(m_complement);
+                    break;
+                case "fichier" : Fichier m_fichier = find.as(Fichier.class);
+                    stringResult = mapper.writeValueAsString(m_fichier);
+                    break;
+                case "matiere" : Matiere m_matiere = find.as(Matiere.class);
+                    stringResult = mapper.writeValueAsString(m_matiere);
+                    break;
+                case "model" :  Model m_modele = find.as(Model.class);
+                    stringResult = mapper.writeValueAsString(m_modele);
+                    break;
+                case "oeuvre" : Oeuvre m_oeuvre = find.as(Oeuvre.class);
+                    stringResult = mapper.writeValueAsString(m_oeuvre);
+                    break;
+                case "oeuvreTraitee" : OeuvreTraitee m_oeuvreTraitee = find.as(OeuvreTraitee.class);
+                    stringResult = mapper.writeValueAsString(m_oeuvreTraitee);
+                    break;
+                case "produit" : Produit m_produit = find.as(Produit.class);
+                    stringResult = mapper.writeValueAsString(m_produit);
+                    break;
+                case "tacheTraitement" : TacheTraitement m_tacheTraitement = find.as(TacheTraitement.class);
+                    stringResult = mapper.writeValueAsString(m_tacheTraitement);
+                    break;
+                case "technique" : Technique m_technique = find.as(Technique.class);
+                    stringResult = mapper.writeValueAsString(m_technique);
+                    break;
+                case "traitement" : Traitement m_traitement = find.as(Traitement.class);
+                    stringResult = mapper.writeValueAsString(m_traitement);
+                    break;
+
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            //return badRequest();
+        }
+
+        return stringResult;
+
+    }
+
 
     public String mapping(Find find, String collection){
 
