c49bfdd3404c7d1fa6ffa16fa188150d7ae1a11c
David <dvd.brtrnd@gmail.com>
2017-03-07 22:37:21 +0100

    save + update (suite)                           insert() dans MongoAccess

--- app/utils/MongoAccess.java
+++ app/utils/MongoAccess.java
@@ -12,229 +12,230 @@ import java.util.regex.Pattern;
 
 public class MongoAccess {
 
-	DB db;
-	Jongo jongo;
-	MongoCollection collec;
-	String token;
+    DB db;
+    Jongo jongo;
+    MongoCollection collec;
+    String token;
 
-	public Jongo connect(String login, String pass, String base, String token) {
+    public Jongo connect(String login, String pass, String base, String token) {
 
-		this.token = token;
+        this.token = token;
 
-		LoadConfig.loadSettings();
+        LoadConfig.loadSettings();
 
-		MongoClientURI uri = new MongoClientURI(String.format("mongodb://%s:%s@%s:%s/%s",
-				login,
-				pass,
-				Settings.getAdresse(),
-				Settings.getPort(),
-				base));
+        MongoClientURI uri = new MongoClientURI(String.format("mongodb://%s:%s@%s:%s/%s",
+                login,
+                pass,
+                Settings.getAdresse(),
+                Settings.getPort(),
+                base));
 
-		MongoClient client = new MongoClient(uri);
-		db = client.getDB(uri.getDatabase());
-		jongo = new Jongo(db);
+        MongoClient client = new MongoClient(uri);
+        db = client.getDB(uri.getDatabase());
+        jongo = new Jongo(db);
 
-		return jongo;
-	}
-	
-	public boolean checkIfExists (String table, String field, String valeur) {
+        return jongo;
+    }
 
-		collec = jongo.getCollection(table);
-		return  collec.findOne(String.format("{\"%s\" : \"%s\"}", field, valeur)).as(Classes.valueOf(table).getUsedClass()) != null;
-		//return  collec.findOne(String.format("{\"%s\" : \"%s\"}", field, valeur)).as(Client.class) != null;
-		
-		
-	}
-	
-    public Find request(String table) {	
-		
-		Find find = null;
-		collec = jongo.getCollection(table);
-		find = collec.find();
+    public boolean checkIfExists (String table, String field, String valeur) {
 
-		return find;
-	}
-    
+        collec = jongo.getCollection(table);
+        return  collec.findOne(String.format("{\"%s\" : \"%s\"}", field, valeur)).as(Classes.valueOf(table).getUsedClass()) != null;
+        //return  collec.findOne(String.format("{\"%s\" : \"%s\"}", field, valeur)).as(Client.class) != null;
+
+
+    }
+
+    public Find request(String table) {
+
+        Find find = null;
+        collec = jongo.getCollection(table);
+        find = collec.find();
+
+        return find;
+    }
+
     public FindOne request(String table, ObjectId id) {
-		
-		FindOne find = null;
-		collec = jongo.getCollection(table);
-		find = collec.findOne("{_id :  #}", id);
 
-		return find;
-	}
+        FindOne find = null;
+        collec = jongo.getCollection(table);
+        find = collec.findOne("{_id :  #}", id);
 
-	public FindOne request(String table, String id) {
+        return find;
+    }
 
-		FindOne find = null;
-		collec = jongo.getCollection(table);
-		find = collec.findOne("{_id :  #}", new ObjectId(id));
+    public FindOne request(String table, String id) {
 
-		return find;
-	}
-    
-    public Find request(String table, Commande commande) {	
-		
-		Find find = null;
-		collec = jongo.getCollection(table);
-		find = collec.find("{commande._id :  #}", commande.get_id_obj());
+        FindOne find = null;
+        collec = jongo.getCollection(table);
+        find = collec.findOne("{_id :  #}", new ObjectId(id));
 
-		return find;
-	}
-    
+        return find;
+    }
+
+    public Find request(String table, Commande commande) {
+
+        Find find = null;
+        collec = jongo.getCollection(table);
+        find = collec.find("{commande._id :  #}", commande.get_id_obj());
+
+        return find;
+    }
+
     public Find request(String table, String field, ObjectId objectId) {
-		
-		Find find = null;
-		collec = jongo.getCollection(table);
-		find = collec.find("{# :  #}", field, objectId);
 
-		return find;
-	}
-    
-    
+        Find find = null;
+        collec = jongo.getCollection(table);
+        find = collec.find("{# :  #}", field, objectId);
+
+        return find;
+    }
+
+
     public Distinct distinct(String table, String distinct, String field, ObjectId objectId) {
-		
-		Distinct find = null;
-		collec = jongo.getCollection(table);
-		find = collec.distinct(distinct).query("{# :  #}", field, objectId);
-		
-		//System.out.println();
 
-		return find;
-	}
-    
+        Distinct find = null;
+        collec = jongo.getCollection(table);
+        find = collec.distinct(distinct).query("{# :  #}", field, objectId);
+
+        //System.out.println();
+
+        return find;
+    }
+
     public Find request(String table, String field, ObjectId objectId, String object) {
-		
-		Find find = null;
-		collec = jongo.getCollection(table);
-		find = collec.find("{# :  #}", field, objectId).projection("{# : 1, _id : 0", object);
 
-		return find;
-	}
-    
-    public Find request(String table, String field, Commande commande) {	
-    	
+        Find find = null;
+        collec = jongo.getCollection(table);
+        find = collec.find("{# :  #}", field, objectId).projection("{# : 1, _id : 0", object);
+
+        return find;
+    }
+
+    public Find request(String table, String field, Commande commande) {
+
 //    	System.out.println(field);
-		
-		Find find = null;
-		collec = jongo.getCollection(table);
-		find = collec.find("{commande_id :  #}", commande.get_id_obj());
 
-		return find;
-	}
-    
-    public Find request(String table, Client client) {	
-		
-		Find find = null;
-		collec = jongo.getCollection(table);
-		find = collec.find("{client._id : #}", client.get_id_obj());
+        Find find = null;
+        collec = jongo.getCollection(table);
+        find = collec.find("{commande_id :  #}", commande.get_id_obj());
 
-		return find;
-	}
-    
-    public Find request(String table, Traitement traitement) {	
-		
-		Find find = null;
-		collec = jongo.getCollection(table);
-		find = collec.find("{traitement : #}", traitement.get_id_obj());
+        return find;
+    }
 
-		return find;
-	}
-	
-	public FindOne request(String table, String field, String valeur) {	
-		
-		FindOne one = null;
-		collec = jongo.getCollection(table);
+    public Find request(String table, Client client) {
 
-		if (field.contains("_id")) {
-			one = collec.findOne(String.format("{\"%s\": # }", field), new ObjectId(valeur));
-		}
-		else {
-			one = collec.findOne(String.format("{\"%s\" : \"%s\"}", field, valeur));
-		}
+        Find find = null;
+        collec = jongo.getCollection(table);
+        find = collec.find("{client._id : #}", client.get_id_obj());
 
-		return one;
-	}
+        return find;
+    }
 
-	public Find requestAll(String table, String field, String valeur) {
+    public Find request(String table, Traitement traitement) {
 
-		Find find = null;
-		collec = jongo.getCollection(table);
+        Find find = null;
+        collec = jongo.getCollection(table);
+        find = collec.find("{traitement : #}", traitement.get_id_obj());
 
-		if (field.contains("_id")) {
-			find = collec.find(String.format("{\"%s\" : # }", field), new ObjectId(valeur));
-		}
-		else {
-			find = collec.find(String.format("{\"%s\" : \"%s\"}", field, valeur));
-		}
+        return find;
+    }
 
-		return find;
-	}
-	public Find requestAll(String table, String field, ObjectId objectId) {
+    public FindOne request(String table, String field, String valeur) {
 
-		Find find = null;
-		collec = jongo.getCollection(table);
-		find = collec.find(String.format("{\"%s\" : %s}", field, objectId));
+        FindOne one = null;
+        collec = jongo.getCollection(table);
 
-		return find;
-	}
+        if (field.contains("_id")) {
+            one = collec.findOne(String.format("{\"%s\": # }", field), new ObjectId(valeur));
+        }
+        else {
+            one = collec.findOne(String.format("{\"%s\" : \"%s\"}", field, valeur));
+        }
 
-	
-    public FindOne request(String table, String field1, String valeur1, String field2, String valeur2) {	
-		
-		FindOne one = null;
-		collec = jongo.getCollection(table);
-		one = collec.findOne(String.format("{\"%s\" : \"%s\", \"%s\" : \"%s\"}", field1, valeur1, field2, valeur2));
+        return one;
+    }
 
-		return one;
-	}
+    public Find requestAll(String table, String field, String valeur) {
 
-	public FindOne request(String table, String field, String valeur, boolean regex) {
+        Find find = null;
+        collec = jongo.getCollection(table);
 
-		FindOne one = null;
-		collec = jongo.getCollection(table);
+        if (field.contains("_id")) {
+            find = collec.find(String.format("{\"%s\" : # }", field), new ObjectId(valeur));
+        }
+        else {
+            find = collec.find(String.format("{\"%s\" : \"%s\"}", field, valeur));
+        }
 
-		String query = String.format("{%s : #}", field);
-		String reg = String.format("^%s.PR.1.JPG", valeur);
+        return find;
+    }
+    public Find requestAll(String table, String field, ObjectId objectId) {
+
+        Find find = null;
+        collec = jongo.getCollection(table);
+        find = collec.find(String.format("{\"%s\" : %s}", field, objectId));
+
+        return find;
+    }
+
+
+    public FindOne request(String table, String field1, String valeur1, String field2, String valeur2) {
+
+        FindOne one = null;
+        collec = jongo.getCollection(table);
+        one = collec.findOne(String.format("{\"%s\" : \"%s\", \"%s\" : \"%s\"}", field1, valeur1, field2, valeur2));
+
+        return one;
+    }
+
+    public FindOne request(String table, String field, String valeur, boolean regex) {
+
+        FindOne one = null;
+        collec = jongo.getCollection(table);
+
+        String query = String.format("{%s : #}", field);
+        String reg = String.format("^%s.PR.1.JPG", valeur);
 
 //		System.out.println(query);
 //		System.out.println(reg);
 
 
-		one = collec.findOne(query, Pattern.compile(reg));
-		//{$regex: #}}", "jo.*"
+        one = collec.findOne(query, Pattern.compile(reg));
+        //{$regex: #}}", "jo.*"
 
 
-		return one;
-	}
-	
-	public void insert (String table, Object m) {
-		collec = jongo.getCollection(table);
-		collec.insert(m);
-		
-	}
-	
-	public Commun save (String table, Commun m) {
+        return one;
+    }
 
-		collec = jongo.getCollection(table);
-		return m;
-		
-	}
+    public void insert (String table, Object m) {
+        collec = jongo.getCollection(table);
+        collec.insert(m);
 
-	public void drop() {
-		collec.drop();
-		
-	}
+    }
 
-	public void update(String table, Commun c) {
-		collec = jongo.getCollection(table);
+    public Commun save (String table, Commun m) {
 
-		collec.update("{_id : #}", c.get_id_obj()).with(c);
-	}
+        collec = jongo.getCollection(table);
+        collec.insert(m);
+        return m;
 
-	public String getToken() {
-		return token;
-	}
+    }
+
+    public void drop() {
+        collec.drop();
+
+    }
+
+    public void update(String table, Commun c) {
+        collec = jongo.getCollection(table);
+
+        collec.update("{_id : #}", c.get_id_obj()).with(c);
+    }
+
+    public String getToken() {
+        return token;
+    }
 
 
 }
